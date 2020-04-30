3818f21ef4babf342944bb0b1d4e6a784463b3dc
Udara Alwis <udara.robotics@gmail.com>
2020-04-30 20:03:38 +0800

    init BlazorApp Demo

---
+++ BlazorClientApp/Pages/Counter.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorClientApp.Pages
+{
+    public class CounterBase : ComponentBase
+    {
+        public int CurrentCount = 0;
+
+        public void IncrementCount()
+        {
+            CurrentCount++;
+        }
+    }
+}
