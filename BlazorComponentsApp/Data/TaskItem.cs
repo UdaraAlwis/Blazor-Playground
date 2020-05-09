4c8a4c672e3fc85b6998b91825dd437abcdc21e6
Udara Alwis <udara.robotics@gmail.com>
2020-05-09 22:47:39 +0800

    tasklist components done

--- BlazorComponentsApp/Data/TaskItem.cs
+++ BlazorComponentsApp/Data/TaskItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,8 @@ namespace BlazorComponentsApp.Data
 {
     public class TaskItem
     {
+        [Required]
         public string TaskTitle { get; set; }
-        public string IsDone { get; set; }
+        public bool IsComplete { get; set; }
     }
 }
