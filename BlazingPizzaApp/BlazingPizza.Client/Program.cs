d5522ef15ece08cb41ea08955d5fd6da695b2feb
Udara Alwis <udara.robotics@gmail.com>
2020-05-17 22:05:43 +0800

    completed till Checkout with Validation

--- BlazingPizzaApp/BlazingPizza.Client/Program.cs
+++ BlazingPizzaApp/BlazingPizza.Client/Program.cs
@@ -14,6 +14,7 @@ namespace BlazingPizza.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<OrderState>();
 
             await builder.Build().RunAsync();
         }
